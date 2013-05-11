453280dce9e28a6b1f877102a82d519937422504
Richard Race <race.richard@gmail.com>
2013-05-12 00:00:29 +0100

    Added working code for num words and for searching by property type

--- app/models/parse_search_string.rb
+++ app/models/parse_search_string.rb
@@ -1,7 +1,33 @@
 class ParseSearchString
 
+  attr_reader :num_words
+
+  def initialize
+    @num_words = {:one => 1,
+                  :two => 2,
+                  :three => 3,
+                  :four => 4,
+                  :five => 5,
+                  :six => 6,
+                  :seven => 7,
+                  :eight => 8}
+  end
+
   def find_bedrooms(search_string)
-    search_string.match(/\d\s(bed)/i).to_s[0].to_i
+    found_bed_count = 0
+    result = search_string.match(/(\d|one|two|three|four|five|six|seven|eight|nine)\s*(bed)/i).to_s
+    if result[0].to_i > 0
+      found_bed_count = result[0].to_i 
+    else
+      @num_words.each_key do |key| 
+        found_bed_count = @num_words[key] if result.include?(key.to_s)
+      end
+    end
+    found_bed_count
+  end
+
+  def find_property_type(search_string)
+    search_string.match(/(house|flat)/i).to_s.downcase
   end
 
 end
