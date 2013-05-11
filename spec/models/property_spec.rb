919aaf8aed9587f1415af93a1ef6961fd2bde366
Richard Race <race.richard@gmail.com>
2013-05-11 22:10:27 +0100

    Added failing test for Properties find_near_by

--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -2,18 +2,19 @@ require 'spec_helper'
 
 describe Property do
   
-  describe "#geocode" do
-    before(:each) do
-      @prop = Property.new(:name => "Sizeable house",
-                           :bedroom_count => 2,
-                           :latitude => 51.501000,
-                           :longitude => -0.142000)
-      @prop.valid?
-    end
+  before(:each) do
+    @prop = Property.new(:name => "Sizeable house",
+                         :bedroom_count => 2,
+                         :latitude => 51.501000,
+                         :longitude => -0.142000)
+    @prop.valid?
+  end
 
-    after(:each) do
-      Property.find(:all).each {|property| property.destroy}
-    end
+  after(:each) do
+    Property.find(:all).each {|property| property.destroy}
+  end
+
+  describe "#geocode" do
 
     it "a Property can have the address generated from lat/long coord" do      
       @prop.address.should_not be_nil
@@ -26,7 +27,7 @@ describe Property do
       found.first.should eq @prop
     end
 
-    it "will get incorrect distance nearby properties within 20km of esher" do
+    it "will get incorrect distance nearby properties within 20km of Esher" do
       location_string = "esher"
       @prop.save
       found = Property.near(location_string, 20, :units => "km", :order => "distance")
@@ -35,5 +36,15 @@ describe Property do
 
   end
 
+  describe "#find_near_by" do
+
+    it "will get no properties within 20km of Esher" do
+      location_string = "esher"
+      @prop.save
+      found = Property.find_near_by(location_string, 20, :units => "km", :order => "distance")
+      found.should be_empty
+    end
+
+  end
 
 end
